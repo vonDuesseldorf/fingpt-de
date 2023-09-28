ddbeab2136f8895641972fd619a6d5dc039805b3
vonDuesseldorf <andreaskunzeduesseldorf@gmail.com>
2023-09-28 12:36:58 +0200

    Next try

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span>Fingpt.de Duesseldorf</span>
+        <span>Fingpt.de</span>
       </a>
     </span>
   )
